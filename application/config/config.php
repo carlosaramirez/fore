56f87441537b0c9c2b00f1b1a592719679bae8df
carlosaramirez <ramirezcarlos_alberto@hotmail.com>
2018-02-01 10:58:33 -0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://softwaredelnea.com.ar/forecast';
+$config['base_url'] = 'http://localhost';
 
 /*
 |--------------------------------------------------------------------------
